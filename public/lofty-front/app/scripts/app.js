9f1fc0f9d900886b9f99ba29d02a098d6d0b3f50
Bruno Ferreira <brunogonncalves@icloud.com>
2014-12-23 16:27:48 -0200

    refactored code and change defaults configs

--- public/lofty-front/app/scripts/app.js
+++ public/lofty-front/app/scripts/app.js
@@ -1,14 +1,72 @@
-(function(document) {
-  'use strict';
+(function (document) {
+    'use strict';
 
-  document.addEventListener('polymer-ready', function() {
-    // Perform some behaviour
-    console.log('Polymer is ready to rock!');
-  });
+    document.addEventListener('polymer-ready', function () {
+        // Perform some behaviour
+        console.log('Polymer is ready to rock!');
+    });
 
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
 
 window.app = 'lofty';
-angular.module('lofty', ['ngMaterial']);
+
+angular.module('lofty', [
+    'ngRoute',
+    'ngCookies',
+    'ngMaterial'
+]).constant('AUTH_EVENTS', {
+    loginSuccess: 'auth-login-success',
+    loginFailed: 'auth-login-failed',
+    logoutSuccess: 'auth-logout-success',
+    sessionTimeout: 'auth-session-timeout',
+    notAuthenticated: 'auth-not-authenticated',
+    notAuthorized: 'auth-not-authorized'
+}).constant('baseurl', function(url){
+    var base = 'http://spring_iatf_zplan/';
+
+    if(url.toString().indexOf('sessions') > -1){
+
+        return base+url;
+
+    } else {
+
+        return base+'api/'+url;
+
+    }
+
+}).run([
+    '$log', '$rootScope', '$location', '$timeout', '$mdSidenav', 'AUTH_EVENTS', 'AuthService',
+    function ($log, $rootScope, $location, $timeout, $mdSidenav, AUTH_EVENTS, AuthService) {
+    $rootScope.$on('$routeChangeStart', function (event, next) {
+
+        AuthService.isAuthenticated().then(function(isAuthenticated){
+            if (!isAuthenticated) {
+                event.preventDefault();
+
+                // user is not not authenticated
+                $rootScope.$broadcast(AUTH_EVENTS.notAuthenticated);
+            }
+        });
+
+    });
+}]).config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
+
+    $routeProvider
+        .when('/', {
+            templateUrl: 'scripts/views/dashboard.html'
+        })
+        .when('/404', {
+            templateUrl: '404.html'
+        })
+        .otherwise('/');
+
+    $httpProvider.interceptors.push([
+        '$injector',
+        function ($injector) {
+            return $injector.get('AuthInterceptor');
+        }
+    ]);
+
+}]);
