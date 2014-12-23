162a8ac04437df009df6d0555a6428485b5fdfde
Bruno Ferreira <brunogonncalves@icloud.com>
2014-12-23 16:32:53 -0200

    created session service

---
+++ public/lofty-front/app/scripts/service/Session.js
@@ -0,0 +1,16 @@
+/**
+ * Created by brunoferreirbrunoa on 12/19/14.
+ */
+(function(app){
+
+    angular.module(app).service('Session', function () {
+        this.create = function (user) {
+            this.user = user;
+        };
+        this.destroy = function () {
+            this.user = null;
+        };
+        return this;
+    });
+
+})(app);
