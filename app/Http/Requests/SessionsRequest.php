45605b77f321fd0f5b1f848c87edb88b94e6404e
Bruno Ferreira <brunogonncalves@icloud.com>
2014-12-23 16:23:54 -0200

    created form request validator

---
+++ app/Http/Requests/SessionsRequest.php
@@ -0,0 +1,23 @@
+<?php namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Response;
+
+Class SessionsRequest extends FormRequest {
+
+    //
+
+    public function rules() {
+
+        return [
+            'email' => 'required',
+            'password'  => 'required'
+        ];
+
+    }
+
+    public function authorize() {
+        return true;
+    }
+
+}
