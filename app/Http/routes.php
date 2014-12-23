439281897f842c98da0c3629c26b928d3a74063c
Bruno Ferreira <brunogonncalves@icloud.com>
2014-12-23 16:54:57 -0200

    changed routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,37 +11,10 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
-
-Route::get('home', 'HomeController@index');
-
-$router->group(['before' => 'auth', 'prefix' => 'api'], function($router)
-{
-
-	Route::controllers([
-		'/'	=> 'ApiController'
-	]);
-
-});
-
 Route::get('login', 'SessionsController@create');
 Route::post('login', 'SessionsController@store');
 Route::get('logout', 'SessionsController@destroy');
 Route::post('logout', 'SessionsController@destroy');
 Route::resource('sessions', 'SessionsController');
 
-/*
-|--------------------------------------------------------------------------
-| Authentication & Password Reset Controllers
-|--------------------------------------------------------------------------
-|
-| These two controllers handle the authentication of the users of your
-| application, as well as the functions necessary for resetting the
-| passwords for your users. You may modify or remove these files.
-|
-*/
-
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
+require __DIR__.'/../project/route.php';
