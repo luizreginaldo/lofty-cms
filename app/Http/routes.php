09ca7762a8a98f76c79a8a554589a1aee53594a1
Bruno Ferreira <brunogonncalves@icloud.com>
2015-01-05 12:21:37 -0200

    fix routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,14 +10,12 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: *");
 
 Route::get('login', 'SessionsController@create');
 Route::post('login', 'SessionsController@store');
 Route::get('logout', 'SessionsController@destroy');
-Route::post('logout', 'SessionsController@destroy');
+Route::delete('sessions', 'SessionsController@destroy');
+
 Route::resource('sessions', 'SessionsController');
 
 Route::get('/', 'WelcomeController@index');
