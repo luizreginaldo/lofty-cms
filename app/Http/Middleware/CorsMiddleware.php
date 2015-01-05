08a2869463242b117ca3330a20eb497dd5a8f56c
Bruno Ferreira <brunogonncalves@icloud.com>
2015-01-05 12:27:18 -0200

    change cors middleware

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,55 @@
+<?php namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Contracts\Routing\Middleware;
+
+class CorsMiddleware implements Middleware {
+
+    /**
+     * The Guard implementation.
+     *
+     * @var Guard
+     */
+    protected $auth;
+
+    /**
+     * Create a new filter instance.
+     *
+     * @param  Guard  $auth
+     * @return void
+     */
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $headers = [
+            'Access-Control-Allow-Origin' => 'application/json',
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization, X-Requested-With',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Max-Age' => 600
+        ];
+
+        $response = $next($request);
+        foreach($headers as $k => $v)
+        {
+            if($response && isset($response->headers)){
+                $response->headers->set($k, $v);
+            }
+        }
+
+        return $response;
+    }
+
+}
