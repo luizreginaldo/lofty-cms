1ea76be01485d850e7f05f51c87d48273ef7648c
Bruno Ferreira <brunogonncalves@icloud.com>
2015-01-06 12:30:06 -0200

    created request interceptor, for process failures and errors responses

---
+++ public/lofty-front/app/scripts/factory/RequestInterceptor.js
@@ -0,0 +1,41 @@
+/**
+ * Created by brunoferreirbrunoa on 12/19/14.
+ */
+(function(app){
+
+    function AuthInterceptor($log, $rootScope, $q, AUTH_EVENTS) {
+        return {
+            response: function(response){
+                if (response.status === 401) {
+                }
+
+                if(response.config.errorKey) {
+
+                    $rootScope.$broadcast('manageErrors', {
+                        key: response.config.errorKey,
+                        errors: response.data && response.data.errors ? response.data.errors : false
+                    });
+
+                }
+
+                return response || $q.when(response);
+            },
+            responseError: function (response) {
+                $rootScope.$broadcast({
+                    401: AUTH_EVENTS.notAuthenticated,
+                    403: AUTH_EVENTS.notAuthorized,
+                    419: AUTH_EVENTS.sessionTimeout,
+                    440: AUTH_EVENTS.sessionTimeout
+                }[response.status], response);
+                return $q.reject(response);
+            }
+        };
+
+    };
+
+    angular.module(app).factory('RequestInterceptor', [
+        '$log', '$rootScope', '$q', 'AUTH_EVENTS',
+        AuthInterceptor
+    ]);
+
+})(app);
