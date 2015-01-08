9fb9d50232fd705552fceec507283b2f547c1a9a
Bruno Ferreira <brunogonncalves@icloud.com>
2015-01-08 18:26:03 +0000

    renamed

--- database/migrations/2015_01_08_182115_create_assigned_roles_table.php
+++ database/migrations/2015_01_08_182115_create_assigned_roles_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AssignedRolesTable extends Migration {
+class CreateAssignedRolesTable extends Migration {
 
 	/**
 	 * Run the migrations.
