0415a27f5df2f66264736a9997486914a53b22d4
Bruno Ferreira <brunogonncalves@icloud.com>
2015-01-08 18:19:30 +0000

    created roles table migration

---
+++ database/migrations/2015_01_08_181401_create_roles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('roles', function ($table) {
+			$table->increments('id')->unsigned();
+			$table->string('name')->unique();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('roles', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
