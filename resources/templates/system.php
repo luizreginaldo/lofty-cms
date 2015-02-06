f872981a437bcd4fb498ab1099e05e40e379a2b7
vagrant <vagrant@vagrant.dev>
2015-02-06 14:28:35 -0200

    load different views from lofty-front folder, based on current laravel 5 environment value

--- resources/templates/system.php
+++ resources/templates/system.php
@@ -1,8 +1,15 @@
 <?php
 
-$url = asset('lofty-front/dist/').'/';
+// get folder based on environment
+function getFolder() {
+    return (env('APP_ENV') != 'production' ? 'app' : 'dist');
+}
 
-echo str_replace(
+// generate default url to frontend assets
+$url = asset('lofty-front/'.getFolder()).'/';
+
+// replace links 
+$page = str_replace(
     [
         'href="',
         'src="'
@@ -11,7 +18,40 @@ echo str_replace(
         'href="'.$url,
         'src="'.$url
     ],
-    File::get(public_path() . '/lofty-front/dist/index.html')
+    File::get(public_path() . '/lofty-front/'.getFolder().'/index.html') // ge view file
 );
 
+// if environment not are production, load custom elements from .tmp
+if(env('APP_ENV') != 'production') {
+    $page = str_replace(
+        [
+            'app/styles',
+            'app/elements/yo-list',
+            'app/elements/yo-greeting'
+        ],
+        [
+            '.tmp/styles',
+            '.tmp/elements/yo-list',
+            '.tmp/elements/yo-greeting'
+        ],
+        $page
+    );
+}
+
+// show html page
+echo $page;
+
 ?>
+<script>
+    //set baseurl with value returned from laravel framework using url() method
+    window.baseurl = function (url) {
+        var base = '<?=url()?>/';
+        return base + url;
+    };
+
+    // set view function for return view
+    window.view = function (file) {
+        var file = file.replace('.html', '').replace('index.php', '');
+        return baseurl('lofty-front/<?=getFolder()?>/scripts/components/') + file + '.html';
+    };
+</script>
